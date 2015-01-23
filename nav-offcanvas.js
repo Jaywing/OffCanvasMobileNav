44af78f4b1e49cd160d77ec092ae5524c306bc74
James Taylor <james@jtwd.co.uk>
2015-01-23 13:15:04 +0000

    Create nav-offcanvas.js

---
+++ nav-offcanvas.js
@@ -0,0 +1,84 @@
+/*
+ * Off-canvas pattern for navigation.
+ *
+ * Dependancies:
+ *   jQuery
+ *   throttledresize.js - https://github.com/louisremi/jquery-smartresize
+ *
+ * Author:
+ *   James Taylor
+ *
+ * Date:
+ *   23/01/2015
+ *
+ * HTML Pattern:
+   <div class="js-container">
+
+     <p class="skip-to-nav"><a href="#global-nav">Menu</a></p>
+
+     <nav id="global-nav" class="nav-global-primary">
+       ... navigation html ...
+     </nav>
+
+   </div>
+ *
+ */
+
+(function (win, doc, $) {
+    'use strict';
+
+    if (!doc.querySelector || !win.addEventListener) {
+        // doesn't cut the mustard.
+        console.log('This browser don\'t cut the Mustard - Exiting off-canvas nav');
+        return;
+    }
+
+    if(!$.event.special.throttledresize) {
+        // throttled resize doesn't exist
+        console.log('throttledresize function is missing - Exiting off-canvas nav');
+        return;
+    }
+
+    var menuActivated = false, // so skiplink event is only applied once
+        configureMenu = function () {
+
+          var toggleclass        = 'slid',
+              $page             = $('.js-container'),
+              $primary          = $('.nav-global-primary'),
+              $skiplink         = $('.skip-to-nav'),
+              $newnav           = $(doc.createElement('div')),
+              $body             = $('body'),
+              mobileBreakPoint  = 623,
+              togglePage = function(e) {
+                  e.preventDefault();
+                  $page.toggleClass(toggleclass);
+              };
+
+          if ($body.width() < mobileBreakPoint) {
+              // If the viewport is small enough, use the off-canvas pattern for navigation
+              if (!$page || !$primary || !$skiplink) {
+                  return;
+              }
+              if(!menuActivated) {
+                $skiplink.click(togglePage);
+              }
+              $newnav.append($primary);
+              $newnav.addClass('js-offcanvas');
+              $skiplink.addClass('persist');
+              $body.append($newnav);
+          } else {
+              // If the viewport is large enough, reset the off-canvas navigation
+              $skiplink.after($primary).removeClass('persist');
+              $page.removeClass(toggleclass);
+          }
+          menuActivated = true;
+        };
+
+    // reconfigure menu when viewport is resized
+    $(window).on("throttledresize", function () {
+      configureMenu();
+    });
+
+    configureMenu();
+
+}(this, this.document, jQuery));
